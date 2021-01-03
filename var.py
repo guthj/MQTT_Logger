893727968f71a46271a8fabf01abbaa9f4449d4f
Jan Guth <Guth_jan@yahoo.com>
2021-01-03 17:23:23 +0100

    save logs 1x/d

--- var.py
+++ var.py
@@ -8,4 +8,4 @@ plants = ["AlocasiaZ", "CalathiaM", "Avocado", "Monstera"]
 plantLog = []
 debuglevel = 5
 debugStr = ["None  :  ", "Error :  ", "Notice:  ", "Info  :  ", "Debug :  "]
-plantLogLenght = 50
+plantLogLenght = 500
